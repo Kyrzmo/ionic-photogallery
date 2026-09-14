3f83fe23c1580a3fa043d412f8255d5afa041fa8
BlueDoesSomething <bennokmadrigal26@gmail.com>
2026-09-14 16:04:17 +0800

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'photo.gallery',
+  appName: 'photo-gallery',
+  webDir: 'dist'
+};
+
+export default config;
---
+++ src/views/HomePage.vue
@@ -0,0 +1,58 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar> <ion-title> My Photo Gallery</ion-title></ion-toolbar>
+    </ion-header>
+    <ion-content class ="ion-padding">
+      <CameraComponent @photo-captured="addPhoto" />
+      <PhotoGalleryComponent :photos="photos" />
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from "@ionic/vue";
+import { ref } from "vue";
+import CameraComponent from "@/components/CameraComponent.vue";
+import PhotoGalleryComponent from "@/components/PhotoGalleryComponent.vue";
+const photos = ref<string[]>([]);
+const addPhoto = (photo: string) => {
+  photos.value.unshift(photo);
+}
+</script>
+
+<style scoped>
+#container {
+  text-align: center;
+  
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+#container strong {
+  font-size: 20px;
+  line-height: 26px;
+}
+
+#container p {
+  font-size: 16px;
+  line-height: 22px;
+  
+  color: #8c8c8c;
+  
+  margin: 0;
+}
+
+#container a {
+  text-decoration: none;
+}
+</style>
